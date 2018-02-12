869d3e66e5887daf1f409d7d47cb8d337fd0a19d
Zachary-Jackson <jackson.zachary.r@gmail.com>
2018-02-12 14:12:29 -0500

    shoppinglist component
    The shoppinglist component has been created. The homepage's html has been edited.

--- shopping-list/src/app/app.module.ts
+++ shopping-list/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppinglistComponent
   ],
   imports: [
     BrowserModule
