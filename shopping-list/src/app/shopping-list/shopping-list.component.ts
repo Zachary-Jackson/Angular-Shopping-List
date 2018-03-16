8b70449efa557bb2280084c5f01c014d0df69cd8
Zachary-Jackson <jackson.zachary.r@gmail.com>
2018-03-15 23:21:43 -0400

    Seperated shoppingList items by group
    The shopping list application now allows food items to be seperated into food groups and presents them seperatly.

--- shopping-list/src/app/shopping-list/shopping-list.component.ts
+++ shopping-list/src/app/shopping-list/shopping-list.component.ts
@@ -12,14 +12,18 @@ export class ShoppingListComponent implements OnInit {
 
   // shoppingList's data is gotten from CartService
   shoppingList: Cart[];
-
   selectedItem: Cart;
+
+  // The following food types are gotten by getGroups() OnInit
+  fruit: Cart[];
+  other: Cart[];
 
 
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
     this.getCart();
+    this.getGroups();
   }
 
   // onSelect takes the value "item" that is given to it from shopping-list.component.html
@@ -28,9 +32,26 @@ export class ShoppingListComponent implements OnInit {
     this.selectedItem = item;
   }
 
+
+  // getGroups sorts shoppingList by food category and fills out the food groups
+  getGroups(): void {
+    this.fruit = []
+    this.other = []
+    for (let item of this.shoppingList) {
+      if (item.group ==='fruit') {
+        this.fruit.push(item)
+      }
+      if (item.group ==='other') {
+        this.other.push(item)
+      }
+    }
+  }
+
   getCart(): void {
     this.cartService.getCart()
       .subscribe(cart => this.shoppingList = cart)
+      this.cartService.getCart()
+      .subscribe(cart => this.fruit = cart)
   }
 
 }
